22ad4eb1ba40fa9722f5d9dd65f7c69a51e3f516
yunsful <bailey05280@naver.com>
2024-11-18 22:05:13 +0900

    [feat] 댓글 조회 dto 추가

---
+++ src/main/java/trend/project/web/dto/commentDTO/CommentDTO.java
@@ -0,0 +1,29 @@
+package trend.project.web.dto.commentDTO;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+public class CommentDTO {
+    
+    @Builder
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor
+    @Getter
+    public static class CommentResponseDTO {
+        
+        private Long id;
+        private String body;
+        private int likesCount;
+        private boolean haveParentComment;
+        private Long parentCommentId;
+        private int depth;
+        private int orderNumber;
+        private String  type;
+        private String memberName;
+        private String companyName;
+        private LocalDateTime updatedAt;
+        private LocalDateTime deletedAt;
+        private boolean deletedTrue;
+    }
+}
