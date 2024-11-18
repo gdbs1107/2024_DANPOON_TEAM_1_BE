7fe2018a89cceeef026bcb2da3da563a01153310
gdbs1107 <gdbs1107@naver.com>
2024-11-18 15:54:36 +0900

    [feat] 회원가입 로직 구현

--- src/main/java/trend/project/repository/MemberRepository.java
+++ src/main/java/trend/project/repository/MemberRepository.java
@@ -7,5 +7,7 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    Boolean existsByUsername(String username);
+
     Optional<Member> findByUsername(String username);
 }
