91a3fdd57317766b8146f794ad4609d11bce056a
gdbs1107 <gdbs1107@naver.com>
2024-11-18 21:37:59 +0900

    [feat] MemberProfileImageService 구현

---
+++ src/main/java/trend/project/service/memberService/MemberProfileImageService.java
@@ -0,0 +1,35 @@
+package trend.project.service.memberService;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+@Service
+public interface MemberProfileImageService {
+
+
+    String upload(MultipartFile multipartFile, String dirName, String username) throws IOException;
+
+    File convert(MultipartFile file) throws IOException;
+
+    String putS3(File uploadFile, String fileName);
+
+    void removeNewFile(File targetFile);
+
+    void saveFileMetadata(String originalFileName, String uniqueFileName, long fileSize, String contentType,
+                          Member findMember);
+
+    byte[] download(String username) throws IOException;
+
+    // memberId로 이미지 조회
+    byte[] downloadImage(Long memberId) throws IOException;
+
+    @Transactional
+    void deleteFile(String username) throws FileNotFoundException;
+
+    String updateProfileImage(MultipartFile newImageFile, String dirName, String username) throws IOException;
+}
