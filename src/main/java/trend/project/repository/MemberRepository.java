e85b36a3d61f06f7611c4edea23bfb71ed152da0
gdbs1107 <gdbs1107@naver.com>
2024-11-18 15:40:36 +0900

    [feat] repository 메서드 구현

--- src/main/java/trend/project/repository/MemberRepository.java
+++ src/main/java/trend/project/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package trend.project.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import trend.project.domain.Member;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByUsername(String username);
 }
