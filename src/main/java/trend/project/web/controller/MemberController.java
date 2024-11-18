1f9d8dd55d3dd187de06a6be46a70d6b91f82d5b
gdbs1107 <gdbs1107@naver.com>
2024-11-18 22:46:04 +0900

    [feat] 회원 아이디&비밀번호 찾기 api 및 회원 삭제 api 구현

--- src/main/java/trend/project/web/controller/MemberController.java
+++ src/main/java/trend/project/web/controller/MemberController.java
@@ -4,13 +4,14 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
 import trend.project.api.ApiResponse;
 import trend.project.service.memberService.MemberService;
 import trend.project.web.dto.memberDTO.MemberJoinDTO;
+import trend.project.web.dto.memberDTO.MemberProfileFindDTO;
+import trend.project.web.dto.memberDTO.MemberProfileUpdateDTO;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,6 +28,73 @@ public class MemberController {
         MemberJoinDTO.MemberJoinResponseDTO response = memberService.joinMember(request);
 
         return ApiResponse.onSuccess(response);
+    }
+
+
+    //프로필 조회 API
+    @Operation(summary = "회원 프로필 조회 API")
+    @GetMapping("/profiles")
+    public void getProfiles(@AuthenticationPrincipal UserDetails userDetails){
+
+    }
+
+
+
+    // 회원 프로필 수정 API
+    @Operation(summary = "회원프로필 수정 API")
+    @PatchMapping("/profiles")
+    public void updateProfile(@AuthenticationPrincipal UserDetails userDetails,
+                              @RequestBody MemberProfileUpdateDTO.MemberProfileUpdateRequestDTO request){
+
+
+    }
+
+
+
+    // 회원 탈퇴 API
+    @Operation(summary = "회원 탈퇴 API")
+    @PatchMapping("")
+    public ApiResponse<String> deleteMember(@AuthenticationPrincipal UserDetails userDetails){
+
+        memberService.deleteMember(userDetails.getUsername());
+        return ApiResponse.onSuccess("성공적으로 삭제 되었습니다");
+
+    }
+
+
+
+    // 아이디 찾기 - 휴대폰 번호로 찾기 (인증 외부 API 필요)
+    @Operation(summary = "휴대폰 번호로 아이디 찾기 API")
+    @GetMapping("/find-usernames/phoneNumbers")
+    public ApiResponse<MemberProfileFindDTO.FindMemberUsernameResponseDTO> getUsernamesWithPhone(MemberProfileFindDTO.FindMemberUsernameWithPhoneNumbersRequestDTO request){
+
+        MemberProfileFindDTO.FindMemberUsernameResponseDTO result = memberService.getUsernamesWithPhone(request);
+
+        return ApiResponse.onSuccess(result);
+
+    }
+
+    // 아이디 찾기 - 이메일로 찾기 (인증 외부 API 필요)
+    @Operation(summary = "이메일로 아이디 찾기 API")
+    @GetMapping("/find-usernames/emails")
+    public ApiResponse<MemberProfileFindDTO.FindMemberUsernameResponseDTO> getUsernamesWithEmails(MemberProfileFindDTO.FindMemberUsernameWithEmailsRequestDTO request){
+
+        MemberProfileFindDTO.FindMemberUsernameResponseDTO result = memberService.getUsernamesWithEmail(request);
+
+        return ApiResponse.onSuccess(result);
+
+    }
+
+
+
+    // 비밀번호 찾기 - 아이디,이름,이메일로 찾기
+    @Operation(summary = "비밀번호 찾기 API")
+    @GetMapping("/find-passwords")
+    public ApiResponse<MemberProfileFindDTO.FindMemberPasswordResponseDTO> getPasswords(MemberProfileFindDTO.FindMemberPasswordRequestDTO request){
+
+        MemberProfileFindDTO.FindMemberPasswordResponseDTO password = memberService.getPassword(request);
+
+        return ApiResponse.onSuccess(password);
     }
 
 
