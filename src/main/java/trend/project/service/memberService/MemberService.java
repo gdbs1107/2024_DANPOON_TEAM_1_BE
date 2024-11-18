fcdd319a364be20eb82cbb27617c632aba13dddd
gdbs1107 <gdbs1107@naver.com>
2024-11-18 22:40:20 +0900

    [feat] 회원 service 코드 구현

--- src/main/java/trend/project/service/memberService/MemberService.java
+++ src/main/java/trend/project/service/memberService/MemberService.java
@@ -1,7 +1,22 @@
 package trend.project.service.memberService;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import trend.project.web.dto.memberDTO.MemberJoinDTO;
+import trend.project.web.dto.memberDTO.MemberProfileFindDTO;
 
 public interface MemberService {
     MemberJoinDTO.MemberJoinResponseDTO joinMember(MemberJoinDTO.MemberJoinRequestDTO request);
+
+    void deleteMember(String username);
+
+    MemberProfileFindDTO.FindMemberUsernameResponseDTO getUsernamesWithPhone(MemberProfileFindDTO.FindMemberUsernameWithPhoneNumbersRequestDTO request);
+
+    MemberProfileFindDTO.FindMemberUsernameResponseDTO getUsernamesWithEmail(MemberProfileFindDTO.FindMemberUsernameWithEmailsRequestDTO request);
+
+    // 비밀번호 재설정 메서드
+    MemberProfileFindDTO.FindMemberPasswordResponseDTO getPassword(MemberProfileFindDTO.FindMemberPasswordRequestDTO request);
+
+    // 매일 자정에 실행
+    @Scheduled(cron = "0 0 0 * * ?")
+    void deleteOldInactiveMembers();
 }
