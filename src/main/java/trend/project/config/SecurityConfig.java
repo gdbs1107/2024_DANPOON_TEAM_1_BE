b8555f7b3562206efd3fc9ed78f83fcf0a69febb
yunsful <bailey05280@naver.com>
2024-11-18 22:12:43 +0900

    [feat] 댓글 조회 API permitAll

--- src/main/java/trend/project/config/SecurityConfig.java
+++ src/main/java/trend/project/config/SecurityConfig.java
@@ -89,6 +89,7 @@ public class SecurityConfig {
                 .requestMatchers("members/join","/login","/companies/join").permitAll()
                 .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**").permitAll()// Swagger 관련 경로를 허용
                 .requestMatchers(HttpMethod.GET, "/plans/{id}").permitAll()
+                .requestMatchers(HttpMethod.GET, "/comments/{id}").permitAll()
                 
                 
                 .anyRequest().authenticated());
