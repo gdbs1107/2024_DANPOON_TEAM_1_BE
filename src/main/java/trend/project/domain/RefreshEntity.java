8cf6a1bcfce5550c198f4cc31586d7b1e478f9f6
gdbs1107 <gdbs1107@naver.com>
2024-11-18 15:11:23 +0900

    [feat] RefreshEntity 구현

---
+++ src/main/java/trend/project/domain/RefreshEntity.java
@@ -0,0 +1,26 @@
+package trend.project.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RefreshEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String refresh;
+
+    private String expiration;
+}
