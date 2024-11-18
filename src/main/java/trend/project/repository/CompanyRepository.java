343ea8ad141f8f3c5da718b7e1996e580a89cbc7
gdbs1107 <gdbs1107@naver.com>
2024-11-18 22:51:29 +0900

    [feat] companyService 코드 완성

--- src/main/java/trend/project/repository/CompanyRepository.java
+++ src/main/java/trend/project/repository/CompanyRepository.java
@@ -1,13 +1,25 @@
 package trend.project.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import trend.project.domain.Company;
+import trend.project.domain.enumClass.Status;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface CompanyRepository extends JpaRepository<Company, Long> {
 
     Optional<Company> findByUsername(String username);
 
+    Optional<Company> findByName(String name);
+
+    Optional<Company> findByCompanyName(String companyName);
+
     Boolean existsByUsername(String username);
+
+    @Query("SELECT c FROM Company c WHERE c.status = :status AND c.inactiveDate <= :cutoffDate")
+    List<Company> findInactiveCompaniesForDeletion(@Param("status") Status status, @Param("cutoffDate") LocalDateTime cutoffDate);
 }
--- src/main/java/trend/project/service/companyService/CompanyService.java
+++ src/main/java/trend/project/service/companyService/CompanyService.java
@@ -1,7 +1,19 @@
 package trend.project.service.companyService;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import trend.project.web.dto.CompanyJoinDTO;
+import trend.project.web.dto.companyDTO.CompanyProfileFindDTO;
 
 public interface CompanyService {
     CompanyJoinDTO.CompanyJoinResponseDTO joinCompany(CompanyJoinDTO.CompanyJoinRequestDTO request);
+
+    void deleteCompany(String username);
+
+    CompanyProfileFindDTO.CompanyUsernameResponseDTO getUsernames(CompanyProfileFindDTO.CompanyUsernameRequestDTO request);
+
+    CompanyProfileFindDTO.CompanyPasswordResponseDTO getPasswords(CompanyProfileFindDTO.CompanyPasswordRequestDTO request);
+
+    // 매일 자정에 실행
+    @Scheduled(cron = "0 0 0 * * ?")
+    void deleteOldInactiveCompanies();
 }
