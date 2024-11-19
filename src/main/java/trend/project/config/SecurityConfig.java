2ecb28a83ef4429d583ffb47784bf3867049fa62
gdbs1107 <gdbs1107@naver.com>
2024-11-19 21:32:59 +0900

    [feat] Ranking 집계 api 구현

--- src/main/java/trend/project/config/SecurityConfig.java
+++ src/main/java/trend/project/config/SecurityConfig.java
@@ -98,7 +98,7 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.GET, "/comments/{id}").permitAll()
                 .requestMatchers(HttpMethod.GET, "/plans/poster/{planId}").permitAll()
                 .requestMatchers(HttpMethod.GET, "/plans/banner/{planId}").permitAll()
-                .requestMatchers("/plans/main/**").permitAll()
+                .requestMatchers("/plans/main/**","/plans/ranking").permitAll()
                 
                 
                 .anyRequest().authenticated());
