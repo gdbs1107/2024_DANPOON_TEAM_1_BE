b92e27fbf40043d99b4c7861b9a586bd32da6392
gdbs1107 <gdbs1107@naver.com>
2024-11-19 22:53:40 +0900

    [feat] HealthCheckController Tag 구현

--- src/main/java/trend/project/web/controller/HealthCheckController.java
+++ src/main/java/trend/project/web/controller/HealthCheckController.java
@@ -1,9 +1,11 @@
 package trend.project.web.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "서버 안정성 체크 API")
 public class HealthCheckController {
 
     @GetMapping("/health")
