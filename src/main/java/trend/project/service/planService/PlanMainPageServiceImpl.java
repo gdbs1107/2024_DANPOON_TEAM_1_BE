d6ffab39cec448c5e5320518a4748e29c7dd4f5f
gdbs1107 <gdbs1107@naver.com>
2024-11-19 16:46:31 +0900

    [feat] Repository 코드 구현

--- src/main/java/trend/project/service/planService/PlanMainPageServiceImpl.java
+++ src/main/java/trend/project/service/planService/PlanMainPageServiceImpl.java
@@ -13,7 +13,7 @@ import java.util.stream.Collectors;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class PlanMainPageServiceImpl {
+public class PlanMainPageServiceImpl implements PlanMainPageService {
 
     private final PlanRepository planRepository;
 
