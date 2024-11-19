b10127f25994806886bb8776b541c1347b4cb596
yunsful <bailey05280@naver.com>
2024-11-19 21:42:11 +0900

    [chore] 댓글 레포지토리 로직 추가

--- src/main/java/trend/project/repository/CommentRepository.java
+++ src/main/java/trend/project/repository/CommentRepository.java
@@ -1,6 +1,9 @@
 package trend.project.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import trend.project.domain.Comment;
 
 import java.util.List;
@@ -8,5 +11,12 @@ import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     
+    @EntityGraph(attributePaths = {"plan","member","company"})
+    @Query("select c from Comment c where c.plan.id = :planId order by c.groups, c.orders")
     Optional<List<Comment>> findByPlanId(Long planId);
+    
+    //가장 큰 order 가져오기
+    @EntityGraph(attributePaths = {"plan","member","company"})
+    @Query("select max(c.orders) from Comment c where c.plan.id = :id and c.groups = :groups")
+    Long findMaxCommentOrder(@Param("id") Long postId, @Param("groups") Long groups);
 }
