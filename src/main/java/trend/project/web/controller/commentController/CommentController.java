25b3c88fd299371bf7e766f62f7fc466cd8071e1
yunsful <bailey05280@naver.com>
2024-11-20 00:01:21 +0900

    [feat] 컨트롤러 추가

--- src/main/java/trend/project/web/controller/commentController/CommentController.java
+++ src/main/java/trend/project/web/controller/commentController/CommentController.java
@@ -3,10 +3,9 @@ package trend.project.web.controller.commentController;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
 import trend.project.api.ApiResponse;
 import trend.project.service.commentService.CommentService;
 import trend.project.web.dto.commentDTO.CommentDTO;
@@ -20,6 +19,16 @@ import java.util.List;
 public class CommentController {
     
     private final CommentService commentService;
+    
+    @Operation(summary = "댓글 작성 API", description = "해당 API는 새로운 댓글을 작성합니다.")
+    @PostMapping("/")
+    public ApiResponse<CommentDTO.CommentCreateResponseDTO> createComment(
+            @PathVariable Long planId,
+            @RequestBody CommentDTO.CommentCreateRequestDTO requestDTO,
+            @AuthenticationPrincipal UserDetails user) {
+        CommentDTO.CommentCreateResponseDTO responseDTO = commentService.createComment(requestDTO, planId, user.getUsername());
+        return ApiResponse.onSuccess(responseDTO);
+    }
     
     @Operation(summary = "기획서 댓글 조회 API", description = "해당 API는 기획서에 달린 댓글들의 정보를 조회합니다.")
     @GetMapping("/")
