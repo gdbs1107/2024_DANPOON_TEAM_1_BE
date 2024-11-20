7ffa1a6968d1afc2e74f5e883061dbc6d826c98d
yunsful <bailey05280@naver.com>
2024-11-20 15:16:07 +0900

    [chore] 엔드포인트 수정에 따른 로직수정

--- src/main/java/trend/project/config/SecurityConfig.java
+++ src/main/java/trend/project/config/SecurityConfig.java
@@ -94,7 +94,8 @@ public class SecurityConfig {
                 .requestMatchers("members/join","/login","/companies/join").permitAll()
                 .requestMatchers("/members/find-usernames/emails","/members/find-usernames/phoneNumbers","/companies/find-passwords","/companies/find-usernames").permitAll()
                 .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**").permitAll()// Swagger 관련 경로를 허용
-                .requestMatchers(HttpMethod.GET, "/plans/{planId}").permitAll()
+                .requestMatchers(HttpMethod.GET, "/plans/detail/{planId}").permitAll()
+                .requestMatchers(HttpMethod.GET, "/plans/detail/{planId}/similar").permitAll()
                 .requestMatchers(HttpMethod.GET, "/comments/{id}").permitAll()
                 .requestMatchers(HttpMethod.GET, "/plans/poster/{planId}").permitAll()
                 .requestMatchers(HttpMethod.GET, "/plans/banner/{planId}").permitAll()
