7206d85c65e0a2e8d8a87ac0f5a10157a73f6d59
yunsful <bailey05280@naver.com>
2024-11-20 11:37:33 +0900

    [test

--- src/main/java/trend/project/repository/MemberRepository.java
+++ src/main/java/trend/project/repository/MemberRepository.java
@@ -12,7 +12,6 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-
     Optional<Member> findByUsername(String username);
 
     Boolean existsByUsername(String username);
