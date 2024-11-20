b6038d89fd3a803b5c6ea24ec6e33112b27a2149
yunsful <bailey05280@naver.com>
2024-11-20 16:23:14 +0900

    [chore] 댓글 조회 엔드포인트 수정

--- src/main/java/trend/project/config/SecurityConfig.java
+++ src/main/java/trend/project/config/SecurityConfig.java
@@ -96,7 +96,7 @@ public class SecurityConfig {
                 .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**").permitAll()// Swagger 관련 경로를 허용
                 .requestMatchers(HttpMethod.GET, "/plans/detail/{planId}").permitAll()
                 .requestMatchers(HttpMethod.GET, "/plans/detail/{planId}/similar").permitAll()
-                .requestMatchers(HttpMethod.GET, "/comments/{id}").permitAll()
+                .requestMatchers(HttpMethod.GET, "/plans/{planId}/comments/").permitAll()
                 .requestMatchers(HttpMethod.GET, "/plans/poster/{planId}").permitAll()
                 .requestMatchers(HttpMethod.GET, "/plans/banner/{planId}").permitAll()
                 .requestMatchers("/plans/main/**","/plans/ranking","/plans/themes/**").permitAll()
