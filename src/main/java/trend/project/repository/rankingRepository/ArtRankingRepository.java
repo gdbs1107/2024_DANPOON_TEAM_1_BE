fce9cc616ccbbf41b752a3dde62e2d97c9030607
gdbs1107 <gdbs1107@naver.com>
2024-11-20 14:11:36 +0900

    [chore] 불필요한 엔티티 제거

--- src/main/java/trend/project/repository/rankingRepository/ArtRankingRepository.java
+++
@@ -1,7 +0,0 @@
-package trend.project.repository.rankingRepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import trend.project.domain.rankingClass.ArtRanking;
-
-public interface ArtRankingRepository extends JpaRepository<ArtRanking, Long> {
-}
--- src/main/java/trend/project/repository/rankingRepository/CommunityRankingRepository.java
+++
@@ -1,7 +0,0 @@
-package trend.project.repository.rankingRepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import trend.project.domain.rankingClass.CommunityRanking;
-
-public interface CommunityRankingRepository extends JpaRepository<CommunityRanking, Long> {
-}
