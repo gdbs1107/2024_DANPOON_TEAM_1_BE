8567493f0102668e46435efc569f5e5367f744d6
yunsful <bailey05280@naver.com>
2024-11-20 15:22:53 +0900

    [chore] 상세 페이지 서비스 분리에 따른 수정 및 같은 지역 기획서 조회 로직 추가

---
+++ src/main/java/trend/project/web/controller/planController/PlanDetailController.java
@@ -0,0 +1,37 @@
+package trend.project.web.controller.planController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import trend.project.api.ApiResponse;
+import trend.project.service.planService.PlanDetailServiceImpl;
+import trend.project.web.dto.planDTO.PlanDetailDTO;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/plans/detail")
+@RequiredArgsConstructor
+@Tag(name = "기획서 상세 조회 페이지 API")
+public class PlanDetailController {
+    
+    private final PlanDetailServiceImpl planDetailService;
+    
+    @Operation(summary = "기획서 상세 조회 API", description = "해당 API는 게시글 상세 정보를 조회합니다.")
+    @GetMapping("/{planId}")
+    public ApiResponse<PlanDetailDTO.PlanDetailResponseDTO> getDetailPlan(@PathVariable Long planId) {
+        PlanDetailDTO.PlanDetailResponseDTO planDetail = planDetailService.getPlanDetail(planId);
+        return ApiResponse.onSuccess(planDetail);
+    }
+    
+    @Operation(summary = "같은 지역 기획서 조회 API", description = "해당 API는 조회 기획서와 같은 지역의 다른 기획서를 조회합니다.")
+    @GetMapping("/{planId}/similar")
+    public ApiResponse<List<PlanDetailDTO.SameProvinceOtherPlanResponseDTO>> getSimilarPlan(@PathVariable Long planId) {
+        List<PlanDetailDTO.SameProvinceOtherPlanResponseDTO> planResponseDTOS = planDetailService.getSameProvinceOtherPlans(planId);
+        return ApiResponse.onSuccess(planResponseDTOS);
+    }
+}
