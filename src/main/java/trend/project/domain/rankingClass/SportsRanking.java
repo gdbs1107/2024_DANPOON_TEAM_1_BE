7130bfa3dc3f8a1dbc3c9b94307a21583cd04451
gdbs1107 <gdbs1107@naver.com>
2024-11-20 13:10:41 +0900

    [feat] 랭킹 엔티티 생성

---
+++ src/main/java/trend/project/domain/rankingClass/SportsRanking.java
@@ -0,0 +1,39 @@
+package trend.project.domain.rankingClass;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+public class SportsRanking {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Schema(description = "순위에 있는 플랜의 식별자 입니다")
+    private Long planId;
+
+    @Schema(description = "게시글 제목 입니다")
+    private String title;
+
+    @Schema(description = "게시글 작성자 닉네임 입니다")
+    private String name;
+
+    @Schema(description = "게시글 좋아요 수 입니다")
+    private Integer likesCount;
+
+    @Schema(description = "게시글 댓글 수 입니다")
+    private Integer commentsCount;
+
+    @Schema(description = "게시글 포스터 이미지 링크입니다")
+    @Column(length = 1000)
+    private String imageLink;
+
+
+}
