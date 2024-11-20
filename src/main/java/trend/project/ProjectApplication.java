8f8023de586c5f352ade1f75c8ba45b374cef13a
gdbs1107 <gdbs1107@naver.com>
2024-11-20 18:42:10 +0900

    [feat] sentry 의존성 주입

--- src/main/java/trend/project/ProjectApplication.java
+++ src/main/java/trend/project/ProjectApplication.java
@@ -1,5 +1,6 @@
 package trend.project;
 
+import io.sentry.Sentry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -12,6 +13,12 @@ public class ProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
+
+		try {
+			throw new Exception("This is a test.");
+		} catch (Exception e) {
+			Sentry.capture(e);
+		}
 	}
 
 }
