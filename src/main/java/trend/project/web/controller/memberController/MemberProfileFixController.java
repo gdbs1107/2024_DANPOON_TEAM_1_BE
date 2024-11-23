c867ca0a459cbc1b4836f6b013284bf6bd63c812
gdbs1107 <gdbs1107@naver.com>
2024-11-23 17:40:34 +0900

    [feat] 개인정보 수정 api 구현

--- src/main/java/trend/project/web/controller/memberController/MemberProfileFixController.java
+++ src/main/java/trend/project/web/controller/memberController/MemberProfileFixController.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+import trend.project.api.ApiResponse;
+import trend.project.service.memberService.MemberProfileFixService;
 import trend.project.validation.annotation.UsernameDuplicate;
 import trend.project.web.dto.memberDTO.MemberProfileFixDTO;
 
@@ -16,21 +18,30 @@ import trend.project.web.dto.memberDTO.MemberProfileFixDTO;
 public class MemberProfileFixController {
 
 
+    private final MemberProfileFixService memberProfileFixService;
+
 
     // 회원 username 수정
     @Operation(summary = "회원 username 수정 API")
     @PatchMapping("/usernames")
-    public void patchUsernames(@AuthenticationPrincipal UserDetails userDetails,
-                               @RequestParam @UsernameDuplicate String newUsername) {
+    public ApiResponse<String> patchUsernames(@AuthenticationPrincipal UserDetails userDetails,
+                                      @RequestParam @UsernameDuplicate String newUsername) {
 
+        String fixedUsername = memberProfileFixService.fixMemberUsername(userDetails.getUsername(), newUsername);
+
+        return ApiResponse.onSuccess(fixedUsername);
     }
 
 
     // 회원 비밀번호 수정
     @Operation(summary = "회원 비밀번호 수정 수정 API")
     @PatchMapping("/passwords")
-    public void patchPasswords(@AuthenticationPrincipal UserDetails userDetails,
+    public ApiResponse<String> patchPasswords(@AuthenticationPrincipal UserDetails userDetails,
                                @RequestParam String newPasswords) {
+
+        memberProfileFixService.fixPassword(userDetails.getUsername(), newPasswords);
+
+        return ApiResponse.onSuccess("비밀번호가 성공적으로 수정되었습니다");
 
     }
 
@@ -38,26 +49,36 @@ public class MemberProfileFixController {
     // 회원 이름 수정
     @Operation(summary = "회원 이름 수정 API")
     @PatchMapping("/names")
-    public void patchNames(@AuthenticationPrincipal UserDetails userDetails,
+    public ApiResponse<String> patchNames(@AuthenticationPrincipal UserDetails userDetails,
                                @RequestParam String newName) {
 
+        String fixedName = memberProfileFixService.fixName(userDetails.getUsername(), newName);
+
+        return ApiResponse.onSuccess(fixedName);
     }
 
 
     // 회원 휴대폰 번호 수정
     @Operation(summary = "회원휴대폰 번호 수정 API")
     @PatchMapping("/phone-numbers")
-    public void patchPhoneNumbers(@AuthenticationPrincipal UserDetails userDetails,
+    public ApiResponse<String> patchPhoneNumbers(@AuthenticationPrincipal UserDetails userDetails,
                                @RequestParam String newPhoneNumber) {
 
+        String fixedPhoneNumber = memberProfileFixService.fixPhoneNumber(userDetails.getUsername(), newPhoneNumber);
+
+        return ApiResponse.onSuccess(fixedPhoneNumber);
     }
 
 
     // 회원 이메일 수정
     @Operation(summary = "회원 이메일 수정 API")
     @PatchMapping("/emails")
-    public void patchEmails(@AuthenticationPrincipal UserDetails userDetails,
+    public ApiResponse<String> patchEmails(@AuthenticationPrincipal UserDetails userDetails,
                                @RequestParam String newEmails) {
+
+        String fixedEmail = memberProfileFixService.fixEmail(userDetails.getUsername(), newEmails);
+
+        return ApiResponse.onSuccess(fixedEmail);
 
     }
 
@@ -65,10 +86,11 @@ public class MemberProfileFixController {
     // 회원 지역 수정
     @Operation(summary = "회원 지역 수정 API")
     @PatchMapping("/address")
-    public void patchAddress(@AuthenticationPrincipal UserDetails userDetails,
+    public ApiResponse<String> patchAddress(@AuthenticationPrincipal UserDetails userDetails,
                              @RequestBody MemberProfileFixDTO.MemberProfileRequestDTO request) {
 
+        memberProfileFixService.fixAddress(userDetails.getUsername(), request);
 
-
+        return ApiResponse.onSuccess("주소가 성공적으로 수정되었습니다");
     }
 }
