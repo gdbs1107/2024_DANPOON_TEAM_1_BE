098d60b4f42ea7a0a89090db2e78a3d43e782e2b
gdbs1107 <gdbs1107@naver.com>
2024-11-23 16:48:27 +0900

    [feat] 회원 정보 수정 API 명세

---
+++ src/main/java/trend/project/web/controller/memberController/MemberProfileFixController.java
@@ -0,0 +1,73 @@
+package trend.project.web.controller.memberController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/members/profiles")
+@RequiredArgsConstructor
+@Tag(name = "회원정보 수정 API")
+public class MemberProfileFixController {
+
+
+
+    // 회원 username 수정
+    @Operation(description = "회원 username 수정 API")
+    @PatchMapping("/usernames")
+    public void patchUsernames(@AuthenticationPrincipal UserDetails userDetails,
+                               @RequestParam String newUsername) {
+
+    }
+
+
+    // 회원 비밀번호 수정
+    @Operation(description = "회원 비밀번호 수정 수정 API")
+    @PatchMapping("/passwords")
+    public void patchPasswords(@AuthenticationPrincipal UserDetails userDetails,
+                               @RequestParam String newPasswords) {
+
+    }
+
+
+    // 회원 이름 수정
+    @Operation(description = "회원 이름 수정 API")
+    @PatchMapping("/names")
+    public void patchNames(@AuthenticationPrincipal UserDetails userDetails,
+                               @RequestParam String newName) {
+
+    }
+
+
+    // 회원 휴대폰 번호 수정
+    @Operation(description = "회원휴대폰 번호 수정 API")
+    @PatchMapping("/phone-numbers")
+    public void patchPhoneNumbers(@AuthenticationPrincipal UserDetails userDetails,
+                               @RequestParam String newPhoneNumber) {
+
+    }
+
+
+    // 회원 이메일 수정
+    @Operation(description = "회원 이메일 수정 API")
+    @PatchMapping("/emails")
+    public void patchEmails(@AuthenticationPrincipal UserDetails userDetails,
+                               @RequestParam String newEmails) {
+
+    }
+
+
+    // 회원 지역 수정
+    @Operation(description = "회원 지역 수정 API")
+    @PatchMapping("/address")
+    public void patchAddress(@AuthenticationPrincipal UserDetails userDetails,
+                               @RequestParam String newAddress) {
+
+    }
+}
