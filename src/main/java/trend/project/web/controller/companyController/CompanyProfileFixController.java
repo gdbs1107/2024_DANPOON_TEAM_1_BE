5ab4cfb3007c476e74edcfe5556ab7fc57cb3735
gdbs1107 <gdbs1107@naver.com>
2024-11-24 01:53:07 +0900

    [feat] 필요한 api 주석으로 구현

---
+++ src/main/java/trend/project/web/controller/companyController/CompanyProfileFixController.java
@@ -0,0 +1,18 @@
+package trend.project.web.controller.companyController;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/company/profile")
+public class CompanyProfileFixController {
+
+
+
+    // companyName 수정
+    // 담당자명 (name) 수정
+    // 비밀번호 수정
+    // phone number 수정
+    // username 수정
+
+}
